10694f6f9673c9e933743ab5b0cde08a618ed2ef
Juriy Bura <juriy.bura@gmail.com>
2020-05-30 15:19:08 +0200

    minor comments

--- server/create-cooldown.js
+++ server/create-cooldown.js
@@ -1,6 +1,8 @@
 const createCooldown = (delay) => {
   let lastUpdateTime = 0;
 
+  // if cooled down - resets the timer
+  // and returns true, otherwise - false
   return () => {
     if (Date.now() - lastUpdateTime > delay) {
       lastUpdateTime = Date.now();
