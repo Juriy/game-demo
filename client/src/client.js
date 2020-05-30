b6acc0b5de5ab50fbe63280248c89410820da0dc
Juriy Bura <juriy.bura@gmail.com>
2020-05-30 21:43:14 +0200

    v3

--- client/src/client.js
+++ client/src/client.js
@@ -26,15 +26,40 @@ const onChatSubmitted = (sock) => (e) => {
   sock.emit('message', text);
 };
 
-const createBoard = (canvas) => {
+const createBoard = (canvas, numCells = 20) => {
   const ctx = canvas.getContext('2d');
+
+  const cellSize = Math.floor(400/numCells);
 
   const fillRect = (x, y, color) => {
     ctx.fillStyle = color;
     ctx.fillRect(x - 10, y - 10, 20, 20);
   };
 
-  return { fillRect };
+  const clear = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  const drawGrid = () => {
+    ctx.beginPath();
+
+    for (let i = 0; i < numCells + 1; i++) {
+      ctx.moveTo(i*cellSize, 0);
+      ctx.lineTo(i*cellSize, numCells*cellSize);
+
+      ctx.moveTo(0, i*cellSize);
+      ctx.lineTo(numCells*cellSize, i*cellSize);
+    }
+
+    ctx.stroke();
+  };
+
+  const reset = () => {
+    clear();
+    drawGrid();
+  };
+
+  return { fillRect, reset };
 };
 
 
@@ -42,12 +67,14 @@ const createBoard = (canvas) => {
 
   const sock = io();
   const canvas = document.querySelector('canvas');
-  const { fillRect } = createBoard(canvas);
+  const { fillRect, reset } = createBoard(canvas);
 
   const onClick = (e) => {
     const { x, y } = getClickCoordinates(canvas, e);
     sock.emit('turn', { x, y });
   };
+
+  reset();
 
   sock.on('message', log);
   sock.on('turn', ({ x, y, color }) => fillRect(x, y, color));
