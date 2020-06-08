38635e80fb5c83060b261a0e117905cddd867841
Juriy Bura <juriy.bura@gmail.com>
2020-06-08 14:26:36 +0200

    rec-start

--- server/server.js
+++ server/server.js
@@ -1,15 +1,1 @@
-const http = require('http');
-const express = require('express');
-
-const app = express();
-app.use(express.static(`${__dirname}/../client`));
-
-const server = http.createServer(app);
-
-server.on('error', (err) => {
-  console.error('Server error:', err);
-});
-
-server.listen(8080, () => {
-  console.log('server started on 8080');
-});
+console.log('server will be here');
