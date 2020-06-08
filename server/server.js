2f0152869cbd3d79ed771ddfeee8a2f87949f46e
Juriy Bura <juriy.bura@gmail.com>
2020-06-08 19:26:07 +0200

    basic server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log('server will be here');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../client`));
+
+const server = http.createServer(app);
+
+server.on('error', (err) => {
+  console.error(err);
+});
+
+server.listen(8080, () => {
+  console.log('server is ready');
+});
